c0adc17c4e5bd4574ca897359a6394c53fad0cd6
sdwchen <861676736@qq.com>
2021-06-22 22:04:50 +0800

    日志配置

--- quartz/src/main/java/com/chen/quartz/job/QuartzJob.java
+++ quartz/src/main/java/com/chen/quartz/job/QuartzJob.java
@@ -1,25 +1,22 @@
 package com.chen.quartz.job;
 
+import com.chen.quartz.service.FeignService;
 import org.quartz.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 // 防止任务并发执行 @DisallowConcurrentExecution
 @DisallowConcurrentExecution
 public class QuartzJob extends QuartzJobBean {
+    @Autowired
+    private FeignService feignService;
     @Override
     protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         JobKey key = jobExecutionContext.getJobDetail().getKey();
-
         JobDataMap dataMap = jobExecutionContext.getJobDetail().getJobDataMap();
-        try {
             System.out.println("key:" + key.getGroup());
-            Thread.sleep(10000);
-
+            System.out.println(feignService.findAllAdmin());
             System.out.println("key:" + key + "   " + "value:" + dataMap);
             // 可以再这里调用第三方接口，管理其他应用的定时任务
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
     }
 }
--- userService/src/main/java/com/chen/user/controller/AdminController.java
+++ userService/src/main/java/com/chen/user/controller/AdminController.java
@@ -3,6 +3,8 @@ package com.chen.user.controller;
 
 import com.chen.user.entity.AdminVO;
 import com.chen.user.service.AdminService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,15 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 
 public class AdminController {
-
+    private final static Logger log = LoggerFactory.getLogger(AdminController.class);
     @Autowired
     private AdminService adminService;
 
     @GetMapping("/findAllAdmin")
     public AdminVO findAllAdmin() {
+        log.info("查询");
         AdminVO adminVO = adminService.findAllAdmin();
 
         System.out.println(adminVO);
+        log.info("用户信息"+adminVO);
         return adminVO;
 
     }
